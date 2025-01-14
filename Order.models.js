82bc63eb05bd68c80d7e9b3edb56c3ad1ff13078
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-14 22:34:43 +0530

    feat: added mongoose model for orders

---
+++ Order.models.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose')
+const OrderSchema=new mongoose.Schema({
+    address:{
+        type:mongoose.Schema.Types.ObjectId,ref:"Address",
+        required:true
+    },
+    items:[{
+        productDetails:{
+            type:mongoose.Schema.Types.ObjectId,ref:"Product",
+            required:true
+        },
+        quantity:{
+            type:Number,
+            required:true
+        }
+    }]
+})
+const Order=mongoose.model("Order",OrderSchema)
+module.exports=Order
